a721ba7de4feb337dd19ff84ea60f587cf69ef32
Douglas Flores <54612161+DouglasFlores-fun@users.noreply.github.com>
2025-03-25 21:58:05 -0300

    Fix: api route dependencies

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToDoGroupController;
+use App\Http\Controllers\ToDoListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
 
 Route::apiResource('groups', ToDoGroupController::class);
 Route::apiResource('tasks', ToDoListController::class)->except(['show']);

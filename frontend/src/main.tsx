48a39383fbec951963b926e4b6efc1f2c9b3ff08
Douglas Flores <54612161+DouglasFlores-fun@users.noreply.github.com>
2025-03-27 22:48:57 -0300

    Fix: Tailwind not working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ToDoList from "./Pages/ToDoList"
+import TailwindTest from "./Pages/TailwindTest"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TailwindTest />
   </StrictMode>,
 )

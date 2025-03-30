d3d64216a77c20a112fdfa3f6e46af991149b9c4
Douglas Flores <54612161+DouglasFlores-fun@users.noreply.github.com>
2025-03-30 01:21:00 -0300

    Fix: Wasnot showing validations from server

--- frontend/src/components/TaskModal.tsx
+++ frontend/src/components/TaskModal.tsx
@@ -15,7 +15,8 @@ interface TaskModal {
 
 
 const TaskModal:React.FC<TaskModal> = (props:TaskModal) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string[]>();
   const [formData, setFormData] = useState<TaskItem>({...props.taskItem, dueDate: convertUTCtoDateLocal(props.taskItem.dueDate)});
   const resetForm = () => {
       setFormData({
@@ -33,28 +34,42 @@ const TaskModal:React.FC<TaskModal> = (props:TaskModal) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const processError = (error)=>{
+    console.log(error);
+    if(error && error.messages){
+      const errors:string[] = Object.entries(error.messages).map(([field, messages]) =>
+        messages.map((msg, index) => msg as string));
+      setErrorMessage(errors);
+    }else{
+      setErrorMessage(["Got error in saving, try again later"]);
+    }
+  }
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    setErrorMessage(undefined);
     modalSpinnerRef.current?.startLoading();
     if(!props.editItem){
       createTask({...formData, dueDate: convertDateLocalToUTC(formData.dueDate)}).then(()=>{
         props.onCompleted();
         modalSpinnerRef.current?.stopLoading(false);
         resetForm();
+        setIsOpen(false);
       }).catch((error)=>{
-        console.log(error);
-        modalSpinnerRef.current?.stopLoading(false);
+        processError(error.response.data);
+        modalSpinnerRef.current?.stopLoading(true);
       });
     }else{
       updateTask({...formData, dueDate: convertDateLocalToUTC(formData.dueDate)}).then(()=>{
         props.onCompleted();
         modalSpinnerRef.current?.stopLoading(false);
+        setIsOpen(false);
       }).catch((error)=>{
-        console.log(error);
-        modalSpinnerRef.current?.stopLoading(false);
+        processError(error.response.data);
+        modalSpinnerRef.current?.stopLoading(true);
       });
     }
-    setIsOpen(false);
+    
   };
 
   return (
@@ -71,6 +86,11 @@ const TaskModal:React.FC<TaskModal> = (props:TaskModal) => {
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
             <h2 className="text-xl font-bold mb-4">{props.title ? props.title : "Add Task"}</h2>
             <form onSubmit={handleSubmit} className="space-y-4">
+              {errorMessage && <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg shadow-md">
+                <p className="font-semibold">Error</p>
+                <ul>{errorMessage.map((error, index) => (<li key={index}>{error}</li>))}</ul>
+              </div>
+              }
               <div>
                 <label className="block text-sm font-medium">Title</label>
                 <input
@@ -89,7 +109,6 @@ const TaskModal:React.FC<TaskModal> = (props:TaskModal) => {
                   value={formData.description}
                   onChange={handleChange}
                   className="w-full mt-1 p-2 border rounded"
-                  required
                 />
               </div>
               <div>
